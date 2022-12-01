6b1e621b2ef68a63a4f491f679c989000d437bce
fos1e2 <jean-marc.brun@cegedim-activ.com>
2022-12-01 17:02:58 +0100

    22 - Spring boot - @SpringBootApplication

--- invoise/src/main/java/com/mycompany/invoise/App.java
+++ invoise/src/main/java/com/mycompany/invoise/App.java
@@ -1,19 +1,16 @@
 package com.mycompany.invoise;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 import com.mycompany.invoise.controller.InvoiceControllerInterface;
 
-@Configuration
-@ComponentScan("com.mycompany.invoise")
+@SpringBootApplication
 public class App {
 
 	public static void main(String[] args) {		
-		@SuppressWarnings("resource")
-		ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
+		ApplicationContext context = SpringApplication.run(App.class);
 		InvoiceControllerInterface invoiceController = context.getBean(InvoiceControllerInterface.class);
 		invoiceController.createInvoice();		
 	}
